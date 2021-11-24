1b6181d2137c532fe466f12ec7b5e80ae0473666
Christopher Choy <cchoy@cs.uchicago.edu>
2021-11-24 16:00:28 -0600

    Update google scrapers and add a formatter for the transparency bundle

---
+++ google/format-transparency-bundle.py
@@ -0,0 +1,39 @@
+#!/bin/usr/python3
+
+import pandas as pd
+''' 
+Formats data found in Google's published google-political-ads-transparency-bundle.zip
+Data is filtered and then written to separate files for usage by other scripts like the web scraper. 
+'''
+
+dir = '/Users/cchoy/Downloads/google-political-ads-transparency-bundle/'
+
+def get_advertiser_ids():
+    advertiser_stats_path = dir + 'google-political-ads-advertiser-stats.csv'
+    outpath = 'outputs/google_political_advertiser_ids.csv'
+    # Filter and format
+    df = pd.read_csv(advertiser_stats_path)
+    df = df.drop(df[df.Regions != 'US'].index) 
+    df = df[['Advertiser_ID','Advertiser_Name','Total_Creatives','Spend_USD','Regions','Elections']]
+    # Write
+    print(df.head())
+    df.to_csv(outpath)
+    print("Number of US Advertisers = ", len(df.index))
+
+def get_creative_urls():
+    ad_urls_path = dir + 'google-political-ads-creative-stats.csv'
+    outpath = 'outputs/google-political-ad_urls.csv'
+    # Filter and format
+    df = pd.read_csv(ad_urls_path, dtype={"Regions": "string", "Spend_USD": "string"})
+    df = df.drop(df[(df.Regions != 'US') & (df.Ad_Type != 'Text')].index)
+    df = df[['Ad_ID', 'Ad_URL', 'Ad_Type', 'Regions', 'Advertiser_ID', 'Advertiser_Name', 'Ad_Campaigns_List', 'Num_of_Days', 
+    'Impressions', 'Spend_USD', 'Last_Served_Timestamp', 'Age_Targeting', 'Gender_Targeting', 'Geo_Targeting_Included',
+    'Geo_Targeting_Excluded', 'Spend_Range_Min_USD', 'Spend_Range_Max_USD']]
+    df = df.head(20_000) # limit number of rows to reduce output filesize
+    # Write
+    print(df.head())
+    df.to_csv(outpath)
+    print("Number of US Text Ads = ", len(df.index))
+
+# get_advertiser_ids()
+# get_creative_urls()
---
+++ google/transparency-report-scraper.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+from bs4 import BeautifulSoup
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+import csv
+
+DEBUG_MODE = False
+
+def scrape(advertiserId):
+    def build_query(advertiserId):
+        baseurl = "https://transparencyreport.google.com/political-ads/advertiser/{aId}?campaign_creatives=start:{start};end:{end};spend:;impressions:;type:3;sort:3&lu=campaign_creatives"
+        start = 1527638400000
+        end   = 1635897600000
+        url = baseurl.format(aId=advertiserId, start=start, end=end)
+        if DEBUG_MODE:
+            print(url)
+        return url
+
+    options = Options()
+    options.headless = True
+    options.add_argument("--window-size=1920,1200")
+    adTexts = []
+    with webdriver.Chrome(options=options) as driver:
+        wait = WebDriverWait(driver, 10) 
+        driver.get(build_query(advertiserId))
+        for _ in range(10): # need to render several times to collect all elements
+            html = driver.execute_script("return document.body.outerHTML;")
+            soup = BeautifulSoup(html, "html.parser")
+        ads = soup.find_all('text-ad')
+        adTexts = [ad.getText() for ad in ads]
+    return adTexts
+
+def main():
+    advIds = ['AR108481940364984320', 'AR105500339708362752', 'AR458000056721604608', 'AR488306308034854912', 'AR249615527784218624', 'AR128018337845215232', \
+        'AR25310001757159424', 'AR194446432348930048', 'AR230475229367894016', 'AR276637125548441600', 'AR432709227698454528', 'AR383080296477622272', 
+        'AR462410610177474560', 'AR157291735423123456', 'AR568293442493349888', 'AR470291909525372928', 'AR101121809528651776'] # up to Pete for America
+    count = 0
+    with open('outputs/tr-adText.csv', 'a') as f:
+            writer = csv.writer(f)
+            writer.writerow(['AdText', 'Category', 'Platform'])
+            for aId in advIds:
+                adTexts = scrape(aId)
+                print(aId, ':', len(adTexts))
+                count += len(adTexts)
+                for ad in adTexts:
+                    if DEBUG_MODE:
+                        print(ad)
+                    writer.writerow([ad, 'political', 'Google'])
+    print("DONE. Ads Scraped: ", count)
+
+main()
