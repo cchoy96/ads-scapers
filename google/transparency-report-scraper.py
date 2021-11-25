02f32f591df6a8f05bbb3009cf108c515c558f14
Christopher Choy <cchoy@cs.uchicago.edu>
2021-11-24 22:53:23 -0600

    cleaned up google output

--- google/transparency-report-scraper.py
+++ google/transparency-report-scraper.py
@@ -34,7 +34,7 @@ def scrape(advertiserId):
             html = driver.execute_script("return document.body.outerHTML;")
             soup = BeautifulSoup(html, "html.parser")
         ads = soup.find_all('text-ad')
-        adTexts = [ad.getText().replace(',','') for ad in ads]
+        adTexts = [ad.getText().strip().encode("ascii","ignore").decode("ascii").replace(',','') for ad in ads]
     return adTexts
 
 def scrape_advertisers(advIds, newFile=False):
@@ -54,6 +54,13 @@ def scrape_advertisers(advIds, newFile=False):
                 writer.writerow([ad, 'political', 'Google'])
     print("DONE. Ads Scraped: ", count)
 
+def clean_duplicates():
+    df = pd.read_csv(outpath)
+    before = len(df.index)
+    df.drop_duplicates(subset=['AdText'], keep='first', inplace=True)
+    print("Dropped {n} duplicate rows.".format(n=before-len(df.index)))
+    df.to_csv(outpath, index=False)
+
 def manual_scrape():
     ''' Provide script with advertiser IDs to scrape in a list'''
     advertiser_ids = ['AR108481940364984320', 'AR105500339708362752', 'AR458000056721604608', 'AR488306308034854912', 'AR249615527784218624', 'AR128018337845215232', \
@@ -62,20 +69,14 @@ def manual_scrape():
     print("num advertisers = ", len(advertiser_ids))
     scrape_advertisers(advertiser_ids)
 
-def csv_scrape():
+def csv_scrape(new):
     '''Provide cleaned csv file from google-transparency-bundle with Advertiser_ID column'''
     df = pd.read_csv(dir + 'google-political-advertiser_ids.csv')
     df = df.sort_values("Spend_USD", ascending=False)
-    advertiser_ids = [id for id in df['Advertiser_ID'].tolist()[61:150]] # paginate through advertiser IDs
+    advertiser_ids = [id for id in df['Advertiser_ID'].tolist()[:150]] # paginate through advertiser IDs
+
     print("num advertisers = ", len(advertiser_ids))
-    scrape_advertisers(advertiser_ids, False)
+    scrape_advertisers(advertiser_ids, new)
 
-def clean_duplicates():
-    df = pd.read_csv(outpath)
-    before = len(df.index)
-    df.drop_duplicates(subset=['AdText'], keep='first', inplace=True)
-    print("Dropped {n} duplicate rows.".format(n=before-len(df.index)))
-    df.to_csv(outpath)
-
-# csv_scrape()
-# clean_duplicates()
+csv_scrape(True)
+clean_duplicates()
